2db704411d4d201fb814e5f3cbb909bbc82c46df
liangmiQwQ <mi_yyds@126.com>
2025-09-30 12:23:05 +0800

    chore: init commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# vue-note
+
+State: **🚧 Work in progress**
+
+vue-note is another style to write Vue. It's light, easy and fast!
+
+## License
+
+[MIT](./LICENSE) License, 2025 - Present [Liang Mi](https://github.com/liangmiqwq)
---
+++ tsdown.config.ts
@@ -0,0 +1,15 @@
+import { join } from 'node:path'
+import process from 'node:process'
+import { defineConfig } from 'tsdown'
+
+export default defineConfig({
+  entry: 'src/index.ts',
+  fixedExtension: true,
+  dts: true,
+  workspace: {
+    include: ['packages/*'],
+    exclude: ['playground'],
+  },
+  tsconfig: join(import.meta.dirname, 'tsconfig.json'),
+  sourcemap: process.env.NODE_ENV === 'development',
+})
