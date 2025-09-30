4b5b87a1ebc2aab8d2b4900f522709f2400e735a
liangmiQwQ <mi_yyds@126.com>
2025-09-30 13:38:05 +0800

    chore: add playground

---
+++ playground/vite.config.ts
@@ -0,0 +1,13 @@
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [
+
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+})
