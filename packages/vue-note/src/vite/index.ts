d4feb98a7921103b627b653eb3c5702a7bd261cd
liangmiQwQ <mi_yyds@126.com>
2025-09-30 18:23:21 +0800

    wip:

--- packages/vue-note/src/vite/index.ts
+++ packages/vue-note/src/vite/index.ts
@@ -2,9 +2,11 @@ import type { PluginOption } from 'vite'
 
 export function VueNote(): PluginOption {
   return {
-    name: 'vue-vine',
-    transform() {
-
+    name: 'vue-note',
+    transform(src, id) {
+      if (id.endsWith('.ts') && src.includes('defineCommentComponents')) {
+        return src.replace('defineCommentComponents', 'defineSomething')
+      }
     },
   }
 }
