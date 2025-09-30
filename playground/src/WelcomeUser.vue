8c6f081e5f3db63133a27cc1cfa41b449633cb7f
liangmiQwQ <mi_yyds@126.com>
2025-09-30 14:50:09 +0800

    wip: add inspector

---
+++ playground/src/WelcomeUser.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div>Good Morning! </div>
+</template>
+
+<style>
+
+</style>
--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,9 +1,14 @@
 import { fileURLToPath } from 'node:url'
+import Vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
+import VitePluginInspect from 'vite-plugin-inspect'
+import { VueNote } from 'vue-note/vite'
 
 export default defineConfig({
   plugins: [
-
+    VitePluginInspect(),
+    VueNote(),
+    Vue(),
   ],
   resolve: {
     alias: {
