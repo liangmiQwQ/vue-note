e8031f9ae681c6ed4271337d76fa7a3b44ff7cc5
liangmiQwQ <mi_yyds@126.com>
2025-10-01 08:39:18 +0800

    wip: config

--- packages/vue-note/src/vite/index.ts
+++ packages/vue-note/src/vite/index.ts
@@ -8,5 +8,24 @@ export function VueNote(): PluginOption {
         return src.replace('defineCommentComponents', 'defineSomething')
       }
     },
+    config(config) { // disable esbuild (oxc) to process typescript
+      if (!config.esbuild) {
+        config.esbuild = {}
+      }
+      config.esbuild.exclude = [
+        ...(
+          config.esbuild.exclude
+            ? (
+                Array.isArray(config.esbuild.exclude)
+                  ? config.esbuild.exclude
+                  : [config.esbuild.exclude]
+              )
+            : []
+        ),
+        /\.ts$/,
+      ]
+
+      return config
+    },
   }
 }
