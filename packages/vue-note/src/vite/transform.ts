748a89790e775e2467ac5c2b0d0dc94f28c3d159
liangmiQwQ <mi_yyds@126.com>
2025-10-02 10:58:59 +0800

    feat: add transform ts to js

--- packages/vue-note/src/vite/transform.ts
+++ packages/vue-note/src/vite/transform.ts
@@ -1,10 +1,11 @@
 import type { Rollup, TransformResult } from 'vite'
 import type { VueNoteQuery } from './query'
+import { rolldownVersion, transformWithEsbuild, transformWithOxc } from 'vite'
 import { parseComponents } from '../compilor/component'
 import { parse } from '../compilor/parse'
 import { resolve } from '../compilor/resolve'
 
-export function transform(src: string, filename: string, ctx: Rollup.TransformPluginContext, query: VueNoteQuery, _ssr: boolean): TransformResult | void {
+export async function transform(src: string, filename: string, ctx: Rollup.TransformPluginContext, query: VueNoteQuery, _ssr: boolean): Promise<TransformResult | void> {
   if (query.raw)
     return
 
@@ -12,8 +13,32 @@ export function transform(src: string, filename: string, ctx: Rollup.TransformPl
   const compiledComponents = parseComponents(filename, fileParseResult.rawComponents)
   const resolvedCode = resolve(fileParseResult.astRestult.program, compiledComponents)
 
-  return {
-    code: resolvedCode,
-    map: null,
+  if (rolldownVersion) {
+    const { code } = await transformWithOxc(
+      resolvedCode,
+      filename,
+      {
+        lang: 'ts',
+      },
+    )
+    return {
+      code,
+      map: null,
+    }
+  }
+  else {
+    const { code } = await transformWithEsbuild(
+      resolvedCode,
+      filename,
+      {
+        target: 'esnext',
+        charset: 'utf8',
+        loader: 'ts',
+      },
+    )
+    return {
+      code,
+      map: null,
+    }
   }
 }
