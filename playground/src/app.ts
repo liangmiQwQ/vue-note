98f0feabec745a48b58a340e94d836298c54fa0a
liangmiQwQ <mi_yyds@126.com>
2025-10-03 19:23:45 +0800

    chore:

--- playground/src/app.ts
+++ playground/src/app.ts
@@ -25,7 +25,7 @@ export default defineCommentComponent(() => {
   const message = ref('Good Morning')
 
   defineTemplate(/* @template
-    <div> {{ message }} 1 2 0</div>
+    <div> {{ message }} </div>
     <AppHome msg="Hello World! " />
     <Counter />
   */)
