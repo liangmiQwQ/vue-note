0054d7e8f495ca0710bf92d5a2114f8d56018ff4
liangmiQwQ <mi_yyds@126.com>
2025-10-03 19:17:38 +0800

    chore:

--- packages/vue-note/src/compilor/utils/hmr.ts
+++ packages/vue-note/src/compilor/utils/hmr.ts
@@ -6,6 +6,23 @@ export function getComponentHmrCode(uniqueId: string, cache: [TransformHashCache
   const _templateChanged = cache[1].template.get(uniqueId) !== cache[0]?.template.get(uniqueId)
   const _scriptChanged = cache[0]?.ast !== cache[1].ast
 
+  /* Orginal
+  return `
+_component.__hmrId = '${uniqueId}'
+typeof __VUE_HMR_RUNTIME__ !== "undefined" && __VUE_HMR_RUNTIME__.createRecord(_component.__hmrId, _component);
+const _templateChanged = ${cache[1].template.get(uniqueId) !== cache[0]?.template.get(uniqueId)}
+const _scriptChanged = ${cache[0]?.ast !== cache[1].ast}
+if (import.meta.hot) {
+  import.meta.hot.accept(() => {
+    if (_scriptChanged) {
+      window.location.reload()
+    } else {
+      __VUE_HMR_RUNTIME__.rerender(_component.__hmrId, _component.render);
+    }
+  })
+}
+`
+  */
   return [
     {
       type: 'ExpressionStatement',
