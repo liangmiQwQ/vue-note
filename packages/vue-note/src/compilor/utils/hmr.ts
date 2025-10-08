2c94b62b079d98b9059fdad35ce0c717c2299bc1
liangmiQwQ <mi_yyds@126.com>
2025-10-09 06:06:04 +0800

    fix:

--- packages/vue-note/src/compilor/utils/hmr.ts
+++ packages/vue-note/src/compilor/utils/hmr.ts
@@ -11,15 +11,7 @@ export function getComponentHmrCode(uniqueId: string, cache: [CacheHash | undefi
 _component.__hmrId = '${uniqueId}';
 _component._templateChanged = ${_templateChanged};
 typeof __VUE_HMR_RUNTIME__ !== "undefined" && __VUE_HMR_RUNTIME__.createRecord(_component.__hmrId, _component);
-__componentsMap.set(_component.__hmrId, _component)
-if (import.meta.hot) {
-  import.meta.hot.accept((mod) => {
-    const __component = mod?.__componentsMap.get(_component.__hmrId)
-    if(__component && __component._templateChanged) {
-      __VUE_HMR_RUNTIME__.rerender(__component.__hmrId, __component.render);
-    }
-  })
-}
+__components.push(_component)
 `
 }
 
@@ -27,15 +19,47 @@ export function wrapperWithHmr(originalCode: string, cache: [CacheHash | undefin
   const __scriptChanged = cache[0]?.ast !== cache[1].ast
 
   return `
-const __componentsMap = new Map();
+const __components = {
+  componentNumber: 2,
+  components: [],
+  _resolve: null,
+  _timeoutId: null,
+  push(e) {
+    this.components.push(e);
+    if (this.components.length >= this.componentNumber && this._resolve) {
+      clearTimeout(this._timeoutId);
+      this._resolve(this.components);
+    }
+  }
+};
+__components.promise = new Promise((resolve, reject) => {
+  __components._resolve = resolve;
+  __components._timeoutId = setTimeout(() => {
+    resolve(null);
+  }, 500);
+  if (__components.components.length >= __components.componentNumber) {
+    resolve(__components.components);
+  }
+});
 ${originalCode}
 export const __scriptChanged = ${__scriptChanged};
 if(import.meta.hot){
-  import.meta.hot.accept((mod)=>{
-    if(mod && mod.__scriptChanged) {
+  import.meta.hot.accept(async (mod)=>{
+    if(!mod) return;
+    if(mod.__scriptChanged) {
+      return window.location.reload();
+    }
+    const components = await mod.__components.promise;
+    if(components){
+      components.forEach((component) => {
+        if(component && component._templateChanged) {
+          __VUE_HMR_RUNTIME__.rerender(component.__hmrId, component.render);
+        }
+      });
+    } else {
       window.location.reload();
     }
   })
 }
-export { __componentsMap }`
+export { __components }`
 }
