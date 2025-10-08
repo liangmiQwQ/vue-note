f6a339789aaa9d980f4cb36059dc321c65501956
liangmiQwQ <mi_yyds@126.com>
2025-10-08 12:26:36 +0800

    chore(eslint): disable autofix for `pnpm/json-enforce-catalog`

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,23 @@
 // @ts-check
 import antfu from '@antfu/eslint-config'
 
-export default antfu(
-  {
-    type: 'lib',
-    pnpm: true,
-  },
-)
+async function getConfig() {
+  const config = await antfu(
+    {
+      type: 'lib',
+      pnpm: true,
+    },
+  )
+
+  // since @antfu/eslint-config doesn't provide override for pnpm option, map manually instead.
+  return config.map((e) => {
+    if (e.name === 'antfu/pnpm/package-json') {
+      // @ts-ignore
+      e.rules['pnpm/json-enforce-catalog'] = ['error', { autofix: false }]
+    }
+
+    return e
+  })
+}
+
+export default getConfig()
