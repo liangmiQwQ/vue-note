13a22ea6489d487d28d37ce419ab96a2cb32991e
liangmiQwQ <mi_yyds@126.com>
2025-10-12 11:39:47 +0800

    refactor: client hmr

--- packages/vue-note/src/compilor/utils/hmr.ts
+++ packages/vue-note/src/compilor/utils/hmr.ts
@@ -1,17 +1,116 @@
+import type { ExportNamedDeclaration, Function } from 'oxc-parser'
+
 export interface CacheHash {
   ast: string
   template: Map<string, string>
 }
 
-export function getComponentHmrCode(uniqueId: string, cache: [CacheHash | undefined, CacheHash]): string {
-  const _templateChanged = !!(cache[0] && cache[0]?.template.get(uniqueId) !== cache[1].template.get(uniqueId))
-  // const range = { start: 0, end: 0 }
+export function getRenderFunctionsObject(renderFunctionsMap: Map<string, Function>, cache: [CacheHash | undefined, CacheHash]): ExportNamedDeclaration {
+  const range = { start: 0, end: 0 }
+  return {
+    type: 'ExportNamedDeclaration',
+    ...range,
+    declaration: {
+      type: 'VariableDeclaration',
+      kind: 'const',
+      ...range,
+      declarations: [
+        {
+          type: 'VariableDeclarator',
+          ...range,
+          id: {
+            type: 'Identifier',
+            ...range,
+            decorators: [],
+            name: '__VUE_HMR_RENDER_FUNCTIONS__',
+            optional: false,
+            typeAnnotation: null,
+          },
+          init: {
+            type: 'ObjectExpression',
+            ...range,
+            properties: Array.from(renderFunctionsMap).map(([key, func]) => {
+              const _templateChanged = !!(cache[0] && cache[0]?.template.get(key) !== cache[1].template.get(key))
 
+              return {
+                type: 'Property',
+                ...range,
+                kind: 'init',
+                key: {
+                  type: 'Literal',
+                  ...range,
+                  value: `${key}`,
+                  raw: `'${key}'`,
+                },
+                value: {
+                  type: 'ObjectExpression',
+                  ...range,
+                  properties: [
+                    {
+                      type: 'Property',
+                      ...range,
+                      kind: 'init',
+                      key: {
+                        type: 'Identifier',
+                        ...range,
+                        decorators: [],
+                        name: 'changed',
+                        optional: false,
+                        typeAnnotation: null,
+                      },
+                      value: {
+                        type: 'Literal',
+                        ...range,
+                        value: _templateChanged,
+                        raw: `${_templateChanged}`,
+                      },
+                      method: false,
+                      shorthand: false,
+                      computed: false,
+                      optional: false,
+                    },
+                    {
+                      type: 'Property',
+                      ...range,
+                      kind: 'init',
+                      key: {
+                        type: 'Identifier',
+                        ...range,
+                        decorators: [],
+                        name: 'render',
+                        optional: false,
+                        typeAnnotation: null,
+                      },
+                      value: { ...func, type: 'FunctionExpression' },
+                      method: false,
+                      shorthand: false,
+                      computed: false,
+                      optional: false,
+                    },
+                  ],
+
+                },
+                method: false,
+                shorthand: false,
+                computed: false,
+                optional: false,
+              }
+            }),
+          },
+        },
+      ],
+    },
+    specifiers: [],
+    source: null,
+    exportKind: 'value',
+    attributes: [],
+  }
+}
+
+export function getComponentHmrCode(uniqueId: string): string {
   return `
 _component.__hmrId = '${uniqueId}';
-_component._templateChanged = ${_templateChanged};
 typeof __VUE_HMR_RUNTIME__ !== "undefined" && __VUE_HMR_RUNTIME__.createRecord(_component.__hmrId, _component);
-__components.push(_component)
 `
 }
 
@@ -19,28 +118,6 @@ export function wrapperWithHmr(originalCode: string, cache: [CacheHash | undefin
   const __scriptChanged = !!(cache[0] && cache[0]?.ast !== cache[1].ast)
 
   return `
-const __components = {
-  componentNumber: 2,
-  components: [],
-  _resolve: null,
-  _timeoutId: null,
-  push(e) {
-    this.components.push(e);
-    if (this.components.length >= this.componentNumber && this._resolve) {
-      clearTimeout(this._timeoutId);
-      this._resolve(this.components);
-    }
-  }
-};
-__components.promise = new Promise((resolve, reject) => {
-  __components._resolve = resolve;
-  __components._timeoutId = setTimeout(() => {
-    resolve(null);
-  }, 500);
-  if (__components.components.length >= __components.componentNumber) {
-    resolve(__components.components);
-  }
-});
 ${originalCode}
 export const __scriptChanged = ${__scriptChanged};
 if(import.meta.hot){
@@ -49,17 +126,12 @@ if(import.meta.hot){
     if(mod.__scriptChanged) {
       return window.location.reload();
     }
-    const components = await mod.__components.promise;
-    if(components){
-      components.forEach((component) => {
-        if(component && component._templateChanged) {
-          __VUE_HMR_RUNTIME__.rerender(component.__hmrId, component.render);
-        }
-      });
-    } else {
-      window.location.reload();
-    }
+    Object.keys(mod.__VUE_HMR_RENDER_FUNCTIONS__).forEach((id) => {
+      const component = mod.__VUE_HMR_RENDER_FUNCTIONS__[id];
+      if(component && component.changed) {
+        __VUE_HMR_RUNTIME__.rerender(id, component.render);
+      }
+    });
   })
-}
-export { __components }`
+}`
 }
