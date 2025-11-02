6cc29c668594b99346f62c27ce3eea90ba7660c3
liangmiQwQ <mi_yyds@126.com>
2025-11-02 18:35:39 +0800

    chore(playground): try using vitejs/devtools

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,4 +1,5 @@
 import { fileURLToPath } from 'node:url'
+import { DevTools } from '@vitejs/devtools'
 import Vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 import VitePluginInspect from 'vite-plugin-inspect'
@@ -6,6 +7,7 @@ import { VueNote } from 'vue-note/vite'
 
 export default defineConfig({
   plugins: [
+    DevTools(),
     VitePluginInspect(),
     VueNote(),
     Vue(),
@@ -13,6 +15,11 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    rolldownOptions: {
+      debug: { },
     },
   },
 })
