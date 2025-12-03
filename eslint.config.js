54b3e15e8e02c0940c61419875e3c5795f05cd35
liangmiQwQ <github@liangmi.dev>
2025-12-03 19:33:36 +0800

    chore: adjust lint config (antfu/eslint-config@15f476575c969b08a04b44c7135ae76c0b8c7adb)

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,9 @@
 // @ts-check
 import antfu from '@antfu/eslint-config'
 
-async function getConfig() {
-  const config = await antfu(
-    {
-      type: 'lib',
-      pnpm: true,
-    },
-  )
-
-  // since @antfu/eslint-config doesn't provide override for pnpm option, map manually instead.
-  // learn more at https://github.com/antfu/pnpm-workspace-utils/pull/23
-  return config.map((e) => {
-    if (e.name === 'antfu/pnpm/package-json') {
-      // @ts-ignore
-      e.rules['pnpm/json-enforce-catalog'] = ['error', { autofix: false }]
-    }
-
-    return e
-  })
-}
-
-export default getConfig()
+export default await antfu(
+  {
+    type: 'lib',
+    pnpm: true,
+  },
+)
